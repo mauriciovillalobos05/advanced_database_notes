f4357dfa4cc992735f64d453bca6944a3088aa2f
Mauricio Gael Villalobos Aguayo <146992863+mauriciovillalobos05@users.noreply.github.com>
2026-05-19 08:49:10 -0600

    session

---
+++ sql_challenges/challenge-12/solution.sql
@@ -0,0 +1,10 @@
+-- EXERCISE 1: Define "Team Velocity"
+
+-- 1. What is the business question?
+
+-- 
+
+-- 2. What is the exact definition? (Include every filter, every join)
+-- 3. What are the edge cases? (NULLs, cancelled tasks, unassigned tasks, etc.)
+-- 4. What is the unit? (Count, percentage, hours, dollars?)
+-- 5. What would make this metric misleading?
